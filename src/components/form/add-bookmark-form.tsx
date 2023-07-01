27b9018a4bcdb3d8e0d833353411ecc821d7969f
MaxHiit <pro.maxcode@gmail.com>
2023-07-01 13:36:05 +0200

    feat(component): form for upload bookmark

---
+++ src/components/form/add-bookmark-form.tsx
@@ -0,0 +1,33 @@
+import { useState, useCallback, type ChangeEvent, type FormEvent } from 'react';
+
+interface AddBookmarkFormProps {
+	onSubmit: (url: string) => void;
+}
+
+export const AddBookmarkForm = ({ onSubmit }: AddBookmarkFormProps) => {
+	const [urlValue, setUrlValue] = useState<string>('');
+
+	const handleUrlChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const newUrl = e.target.value;
+
+		setUrlValue(newUrl);
+	};
+
+	const handleSubmit = useCallback(
+		(e: FormEvent<HTMLFormElement>) => {
+			e.preventDefault();
+			if (!urlValue) return;
+
+			onSubmit(urlValue);
+			setUrlValue('');
+		},
+		[onSubmit, urlValue]
+	);
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input type='text' name='url' required value={urlValue} onChange={handleUrlChange} />
+			<button type='submit'>Search</button>
+		</form>
+	);
+};
