998ce2782f875a757c51fccd9de8cd29e6acf928
MaxHiit <pro.maxcode@gmail.com>
2023-07-01 13:38:28 +0200

    feat(type): generic type

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export type BookmarkType = {
+	title: string;
+	html: string;
+	url: string;
+	author_name: string;
+	upload_date: string;
+	width?: number;
+	height?: number;
+	duration?: number;
+	created_at: string;
+};
